d6b414d18734d38f563504a038f141bdec945f9d
Puchana <puchana123@gmail.com>
2024-07-21 14:39:10 +0700

    feat: add function test case topics

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,66 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
+    suite('/api/solve', () => {
+        // #1
+        test('Sovle a puzzle with valid puzzle string', () => {
+            assert.fail();
+        });
+        // #2
+        test('Sovle a puzzle with missing puzzle string', () => {
+            assert.fail();
+        });
+        // #3
+        test('Sovle a puzzle with invalid characters', () => {
+            assert.fail();
+        });
+        // #4
+        test('Sovle a puzzle with incorrect length', () => {
+            assert.fail();
+        });
+        // #5
+        test('Solve a puzzle that cannot be solved', () => {
+            assert.fail();
+        });
+    });
 
+    suite('/api/check', () => {
+        // #6
+        test('Check a puzzle placement with all fields', () => {
+            assert.fail();
+        });
+        // #7
+        test('Check a puzzle placement with single placement conflict', () => {
+            assert.fail();
+        });
+        // #8
+        test('Check a puzzle placement with nultiple placement conflict', () => {
+            assert.fail();
+        });
+        // #9
+        test('Check a puzzle placement with all placment conflict', () => {
+            assert.fail();
+        });
+        // #10
+        test('Check a puzzle placement with missing required fileds', () => {
+            assert.fail();
+        })
+        // #11
+        test('Check a puzzle placement with invalid characters', () => {
+            assert.fail();
+        });
+        // #12
+        test('Check a puzzle placement with incorrect length', () => {
+            assert.fail();
+        });
+        // #13
+        test('Check a puzzle placement with invalid placement coordinate', () => {
+            assert.fail();
+        });
+        // #14
+        test('Check a puzzle placement with invalid placement value', () => {
+            assert.fail();
+        });
+    })
 });
 

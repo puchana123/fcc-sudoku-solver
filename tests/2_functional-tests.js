9d35b9a70d0fee9be5aa00db0a851bfe8fa84e66
Puchana <puchana123@gmail.com>
2024-07-21 14:56:28 +0700

    feat: add functional test case for /api/solve

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,24 +8,84 @@ chai.use(chaiHttp);
 suite('Functional Tests', () => {
     suite('/api/solve', () => {
         // #1
-        test('Sovle a puzzle with valid puzzle string', () => {
-            assert.fail();
+        test('Sovle a puzzle with valid puzzle string', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/solve')
+                .send({
+                    puzzle: '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200, 'Api response correctly');
+                    assert.isObject(res.body, 'Response as Object');
+                    assert.equal(res.body.solution, '135762984946381257728459613694517832812936745357824196473298561581673429269145378', 'get correct solution');
+                    done();
+                })
         });
         // #2
-        test('Sovle a puzzle with missing puzzle string', () => {
-            assert.fail();
+        test('Sovle a puzzle with missing puzzle string', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/solve')
+                .send({
+                    puzzle: '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....9.6.14.37.'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200, 'Api response correctly');
+                    assert.isObject(res.body, 'Response as Object');
+                    assert.equal(res.body.solution, '135762984946381257728459613694517832812936745357824196473298561581673429269145378', 'get correct solution');
+                    done();
+                })
         });
         // #3
-        test('Sovle a puzzle with invalid characters', () => {
-            assert.fail();
+        test('Sovle a puzzle with invalid characters', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/solve')
+                .send({
+                    puzzle: '1.5a.2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....9.6.14.37.'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200, 'Api response correctly');
+                    assert.isObject(res.body, 'Response as Object');
+                    assert.equal(res.body.error, 'Invalid characters in puzzle', 'get error message correctly');
+                    done();
+                })
         });
         // #4
-        test('Sovle a puzzle with incorrect length', () => {
-            assert.fail();
+        test('Sovle a puzzle with incorrect length', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/solve')
+                .send({
+                    puzzle: '1.5.2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....9.6.14.37.'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200, 'Api response correctly');
+                    assert.isObject(res.body, 'Response as Object');
+                    assert.equal(res.body.error, 'Expected puzzle to be 81 characters long', 'get error message correctly');
+                    done();
+                })
         });
         // #5
-        test('Solve a puzzle that cannot be solved', () => {
-            assert.fail();
+        test('Solve a puzzle that cannot be solved', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/solve')
+                .send({
+                    puzzle: '1.55.2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....9.6.14.37.'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200, 'Api response correctly');
+                    assert.isObject(res.body, 'Response as Object');
+                    assert.equal(res.body.error, 'Puzzle cannot be solved', 'get error message correctly');
+                    done();
+                })
         });
     });
 
